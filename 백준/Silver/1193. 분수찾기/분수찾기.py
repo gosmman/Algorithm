6a5f9519be3a735d094c26c3334280fcb3c00644
gosmman <151786564+gosmman@users.noreply.github.com>
2023-11-26 21:05:13 +0900

    [Silver V] Title: 분수찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,16 @@
+x = int(input())
+
+line = 1
+while x > line:
+    x -= line
+    line += 1
+
+if line % 2 == 0:
+    numerator = x
+    denominator = line - x + 1
+else:
+    numerator = line - x + 1
+    denominator = x
+
+result = f"{numerator}/{denominator}"
+print(result)
