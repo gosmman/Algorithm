0e527f9fe12c5dcf272544564fe200bf7f48e30c
gosmman <151786564+gosmman@users.noreply.github.com>
2023-12-02 22:31:35 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,5 +1,11 @@
-N = list(map(int,input().split()))
-M = [1,1,2,2,2,8]
+my_list = []
+a = list(map(int, input().split()))
+my_list.append(a)
+chess_list = [1, 1, 2, 2, 2, 8]
+total_list = []
 
-for i in range(6):
-    print(M[i]-N[i], end=' ')
+for i in range(len(a)):
+    total_list.append(chess_list[i] - my_list[0][i])
+
+for value in total_list:
+    print(value, end=" ")
